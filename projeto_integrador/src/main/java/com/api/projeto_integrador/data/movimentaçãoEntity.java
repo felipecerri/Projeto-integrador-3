df98aed922f66dfc405f43985af84f564810b701
Felipe Assunção <felipecerri10@hotmail.com>
2024-01-17 16:52:11 -0300

    Validação para cadastrar movimentação no projeto java

--- projeto_integrador/src/main/java/com/api/projeto_integrador/data/movimentaçãoEntity.java
+++ projeto_integrador/src/main/java/com/api/projeto_integrador/data/movimentaçãoEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,7 +16,11 @@ public class movimentaçãoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    
+    @Size(min=2, message="Digite uma descrição com no minimo 2 caracteres")
     private String descr;
+    
+    @Min(0)
     private double valor;
     private String data;
     private boolean entrada;
