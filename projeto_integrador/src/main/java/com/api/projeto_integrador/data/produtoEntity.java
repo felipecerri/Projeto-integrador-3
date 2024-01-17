9596b144d022b1e6ed9489a40e7d107e51fbac34
Felipe Assunção <felipecerri10@hotmail.com>
2024-01-17 16:48:15 -0300

    Validação para cadastrar produto no projeto java

--- projeto_integrador/src/main/java/com/api/projeto_integrador/data/produtoEntity.java
+++ projeto_integrador/src/main/java/com/api/projeto_integrador/data/produtoEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,11 +16,21 @@ public class produtoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    
+    @Size(min=2, message="Digite um nome com no minimo 2 caracteres")
     private String nome;
+    @Size(min=2, message="Digite um nome com no tipo 2 caracteres")
     private String tipo;
+    
+    @Size(min=2, message="Digite um nome com no marca 2 caracteres")
     private String marca;
+    
+    @Min(0)
     private double valor;
+    
     private String data;
+    
+    @Min(0)
     private Integer quantidade;
     
     
